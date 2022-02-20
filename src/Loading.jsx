eb5e5abd3b9b2831ad6939f38ef6286b7f53a276
Ssingh@1208 <80781404+salonisingh7641@users.noreply.github.com>
2022-02-20 19:44:28 +0530

    Add files via upload

---
+++ src/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Loader.css';
+function Loading(props) {
+    return (
+        <>
+            <div className="container">
+                <div className="dash uno"></div>
+                <div className="dash dos"></div>
+                <div className="dash tres"></div>
+                <div className="dash cuatro"></div>
+            </div>
+        </>
+    );
+};
+
+export default Loading;
